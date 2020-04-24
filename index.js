19f62cc94c102fce7c8b9614758f0d68db391960
ChenyoisRita <ychen386@jhu.edu>
2020-04-24 17:45:56 -0400

    fix 500 bugs

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
+mongoose.Promise = global.Promise;
+
 const options = {
     useUnifiedTopology: true,
     useNewUrlParser: true
@@ -42,7 +44,7 @@ if(process.env.NODE_ENV === 'production') {
     //if it doesn't recognize the route
     const path = require('path');
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
