770a9b861bf1ad015b6b17706e117365876276d7
ChenyoisRita <ychen386@jhu.edu>
2020-04-26 01:18:39 -0400

    fixing property names

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -45,7 +45,7 @@ class SurveyForm extends Component {
 function validate(values) {
     const errors = {};
 
-    errors.emails = validateEmails(values.emails || '');
+    errors.recipients = validateEmails(values.recipients || '');
 
     _.each(formFields, ({ name, noValueError })=> {
         if(!values[name]) {
--- client/src/components/surveys/formFields.js
+++ client/src/components/surveys/formFields.js
@@ -2,5 +2,5 @@ export default [
     {label: 'Campaign Title', name: 'title', noValueError: 'You must provide a title!'},
     {label: 'Subject Line', name: 'subject', noValueError: 'You must provide a subject!'},
     {label: 'Email Body', name: 'body', noValueError: 'You must provide a email!'},
-    {label: 'Recipient List', name: 'emails', noValueError: 'You must provide a recipient list!'}
+    {label: 'Recipient List', name: 'recipients', noValueError: 'You must provide a recipient list!'}
 ];
